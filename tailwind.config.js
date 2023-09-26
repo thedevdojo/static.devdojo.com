198792e318af7f4b2812aede3275793cfde6115f
Tony Lea <tnylea@gmail.com>
2023-09-26 16:12:50 -0400

    making changes to homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,32 @@ module.exports = {
             animation: {
                 marquee: 'marquee 25s linear infinite',
                 marquee2: 'marquee2 25s linear infinite',
-              },
-              keyframes: {
+            },
+            keyframes: {
                 marquee: {
-                  '0%': { transform: 'translateX(0%)' },
-                  '100%': { transform: 'translateX(100%)' },
+                    '0%': {
+                        transform: 'translateX(0%)'
+                    },
+                    '100%': {
+                        transform: 'translateX(100%)'
+                    },
                 },
                 marquee2: {
-                  '0%': { transform: 'translateX(-100%)' },
-                  '100%': { transform: 'translateX(0%)' },
+                    '0%': {
+                        transform: 'translateX(-100%)'
+                    },
+                    '100%': {
+                        transform: 'translateX(0%)'
+                    },
                 },
-              },
+            },
+            'screens' : {
+              'h-mini': { 'raw': '(max-height: 720px)' },
+              'h-small': { 'raw': '(max-height: 825px)' },
+            }
         },
     },
     plugins: [
-      require('@tailwindcss/typography'),
+        require('@tailwindcss/typography'),
     ],
 }
